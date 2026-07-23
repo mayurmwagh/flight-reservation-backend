a9635169eb26e8629569f9bbb0ee641d0889952a
mayurwagh <mayurwagh1412@gmail.com>
2026-07-23 17:54:28 +0530

    pipeline udpated

--- backend-deployment.groovy
+++ backend-deployment.groovy
@@ -1,32 +1,32 @@
 pipeline {
     agent none
+
     stages {
-        stage('code-checkot'){
-            steps{
+
+        stage('code-checkout') {
+            agent any
+            steps {
                 git branch: 'main',
                     url: 'https://github.com/mayurmwagh/flight-reservation-backend.git'
             }
         }
-        stage('build-stage'){
-            
-                agent {
-                    docker {
-                        image 'maven:3.9.11-eclipse-temurin-21'
-                        
-                        reuseNode true
-                        }
-                    }
-            steps{    
-                sh ''' 
-                     sudo apt-get update && 
-                     sudo apt-get install -y maven
-                     java -version
-                     javac -version
-                     mvn -version
-                     mvn clean package
+
+        stage('build-stage') {
+            agent {
+                docker {
+                    image 'maven:3.9.11-eclipse-temurin-21'
+                    reuseNode true
+                }
+            }
+
+            steps {
+                sh '''
+                    java -version
+                    javac -version
+                    mvn -version
+                    mvn clean package
                 '''
             }
         }
     }
-
 }
