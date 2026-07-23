30a5641cebed668896b9b167ec9f96f6b3f27a8e
mayurwagh <mayurwagh1412@gmail.com>
2026-07-23 16:51:32 +0530

    pipeline is added

---
+++ backend-deployment.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any 
+    stages {
+        stage('code-checkot'){
+            steps{
+                git branch: 'main',
+                    url: 'https://github.com/mayurmwagh/flight-reservation-backend.git'
+            }
+        }
+        stage('build-stage'){
+            steps{
+                agent {
+                    docker {
+                        image 'maven:3.9.9-eclipse-temurin-17'
+                        reuseNode true
+                        }
+                    }
+                sh 'mvn clean package'
+            }
+        }
+    }
+
+}
